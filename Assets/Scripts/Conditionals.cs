22380b4a6ef8e94a45434f9817ec1725ee5a7e12
Mark Ballinger <markballinger@optusnet.coml.au>
2022-06-08 20:18:51 +1000

    added if statements
    boolean and is not true

--- Assets/Scripts/Conditionals.cs
+++ Assets/Scripts/Conditionals.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class Conditionals : MonoBehaviour
 {
+    //Variables go here
+    [SerializeField]
+    string playerAction = "Default";
+    [SerializeField]
+    bool testbool = true;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //everything in these brackets is run whn the game starts
+        testbool = playerAction == "Attack";
+        // another way to do the same above statement
+        if(playerAction == "Attack")
+        {
+            Debug.Log("then this happens");
+        }
+        //another way to above statement is with != 
+        if(playerAction != "Attack")
+        {
+            Debug.Log("then this happens");
+        }
+        if (testbool)
+        {
+            Debug.Log("Then this happens");
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // everything in these brackets is callled everyframe
     }
 }
