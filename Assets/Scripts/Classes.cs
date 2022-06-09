684a2b2f8e6b0b79d1258ce18d522d038a942e5f
Mark Ballinger <markballinger@optusnet.coml.au>
2022-06-09 16:03:08 +1000

    Classes added

---
+++ Assets/Scripts/Classes.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Classes : MonoBehaviour
+{
+    //Variables go here
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Everything in these brackets is run when the game starts
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Everything in these brackets is run every frame of the game
+    }
+}
