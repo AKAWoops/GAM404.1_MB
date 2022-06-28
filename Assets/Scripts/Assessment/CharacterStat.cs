b778d0c2c3ab16cc0d10ebc214e690b93889c93f
Mark Ballinger <markballinger@optusnet.coml.au>
2022-06-28 17:18:51 +1000

    added isDirty
    to let you know when it has had a modifier added

--- Assets/Scripts/Assessment/CharacterStat.cs
+++ Assets/Scripts/Assessment/CharacterStat.cs
@@ -1,16 +1,34 @@
+using System;
 using System.Collections.Generic;
-
 
 public class CharacterStat 
 {
     public float BaseValue;
+
+    public float Value
+    {
+        get
+        {
+            if (isDirty)
+            {
+                _value = CalculateFinalValue();
+                isDirty = false;
+            }
+            return _value;
+        }
+    }
+
+    private bool isDirty = true;
+    private float _value;
 
     private readonly List<StatModifier> statModifiers;
 
     public CharacterStat(float baseValue)
     {
         BaseValue = baseValue;
+        statModifiers = new List<StatModifier>();
     }
+    /*
     void someFunc()
     {
         statModifiers = null;
@@ -18,6 +36,30 @@ public class CharacterStat
 
         statModifiers[0] = null;
         statModifiers.Add(new StatModifier());
+    */
+    public void AddModifier(StatModifier mod)
+    {
+        // is diurty added and set to truwe so we know modifiers have been added
+        isDirty = true;
+        statModifiers.Add(mod);
+    }
 
+    public bool RemoveModifier(StatModifier mod)
+    {
+        return statModifiers.Remove(mod);
+    }
+
+    private float CalculateFinalValue()
+    {
+        // isdiurty added to let you know a modifier has been added same as above
+        isDirty = true;
+        float finalValue = BaseValue;
+
+        for (int i = 0; i < statModifiers.Count; i++)
+        {
+            finalValue += statModifiers[i].Value;
+        }
+        // 12.0001f != 12f
+        return (float)Math.Round(finalValue 4);
     }
 }
--- Assets/Scripts/Assessment/StatModifier.cs
+++ Assets/Scripts/Assessment/StatModifier.cs
@@ -1,8 +1,18 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 
+public enum StatModType
+{
+    Flat,
+    Percent,
+}
 public class StatModifier 
 {
-   
+    public readonly float Value;
+    public readonly StatModType Type;
+
+
+    public StatModifier(float value, StatModType type)
+    {
+        Value = value;
+        Type = type;
+    }
 }
