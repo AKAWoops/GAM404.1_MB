c8c5ac530bdc248b4d726d633182163b09cab6e2
Mark Ballinger <markballinger@optusnet.coml.au>
2022-06-28 17:28:19 +1000

    Added mod.value
    added percentage and flat modifier values to be calculated

--- Assets/Scripts/Assessment/CharacterStat.cs
+++ Assets/Scripts/Assessment/CharacterStat.cs
@@ -51,15 +51,26 @@ public class CharacterStat
 
     private float CalculateFinalValue()
     {
-        // isdiurty added to let you know a modifier has been added same as above
+        // isdiurty added to let you know a modifier has been added same as above 
+        // no flat modifiers can be added 
         isDirty = true;
         float finalValue = BaseValue;
 
         for (int i = 0; i < statModifiers.Count; i++)
         {
-            finalValue += statModifiers[i].Value;
+            // this below line and if statement is to help with adealing with each type in a different way :-)
+            StatModifier mod = statModifiers[i];
+            // if it a flat modifier we dow what we are already doing
+            if (mod.Type == StatModType.Flat)
+            {
+            finalValue += mod.Value;
+            }//but if its a percentage modifier calculaton in a different is needed
+            else if (mod.Type == StatModType.Percent)
+            {// increases by 10% moifier +10% value in code = 0.1 value will equal 0.1:-) 1.1 = 110% with 0.1 value
+                finalValue *= 1 + mod.Value;
+            }
         }
         // 12.0001f != 12f
-        return (float)Math.Round(finalValue 4);
+        return (float)Math.Round(finalValue, 4);
     }
 }
