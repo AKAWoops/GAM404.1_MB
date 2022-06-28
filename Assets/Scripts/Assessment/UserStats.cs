5b56cade7e459200134148cff477cc6dd6ee27d4
Mark Ballinger <markballinger@optusnet.coml.au>
2022-06-29 09:33:26 +1000

    Added player controller and camera controller
    Added User stats totally changed mind on this project willl now have a basic 3d component

--- Assets/Scripts/Assessment/UserStats.cs
+++ Assets/Scripts/Assessment/UserStats.cs
@@ -2,8 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Assessment1 : MonoBehaviour
+public class UserStats : MonoBehaviour
 {
+    public string username = "player name";
+    public int level;
     // Start is called before the first frame update
     void Start()
     {
