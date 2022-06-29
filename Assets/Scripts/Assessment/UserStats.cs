974b4eaf617ed041cb8c5638ae980fab4421cb9c
Mark Ballinger <markballinger@optusnet.coml.au>
2022-06-29 10:02:00 +1000

    User stats
    added dodge attack power,speed, mana & HP

--- Assets/Scripts/Assessment/UserStats.cs
+++ Assets/Scripts/Assessment/UserStats.cs
@@ -4,8 +4,27 @@ using UnityEngine;
 
 public class UserStats : MonoBehaviour
 {
-    public string username = "player name";
+    //enter Username variable
+    public string username;
+    //current level if i get that far 
     public int level;
+    //Health Points
+    public float CurHP;
+    public float maxHP;
+    //Mana
+    public float curMana;
+    public float maxMana;
+    //Attack Power
+    public float baseAttackPower;
+    public float curAttackPower;
+    //Attack Speed
+    public float baseAttacSpeed;
+    public float curAttackSpeed;
+    //Dodge
+    public float baseDodge;
+    public float curDodge;
+
+
     // Start is called before the first frame update
     void Start()
     {
